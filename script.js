9c84fc5f569969f1d28fa4c2fe7a93bdade813c2
benti <bentivegna666@gmail.com>
2023-12-06 18:59:02 -0300

    Mouse features done, logic done

--- script.js
+++ script.js
@@ -1,18 +1,42 @@
 window.onload = quadrados ();
 
-function quadrados () {
+function test (){
+    console.log("oi");
+}
+
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
+
+function quadrados (x) {
     let gridsize = document.getElementById('gridsize').value;
     let gridcounter = gridsize ** 2;
-    removegrid();
+    let backgroundcolor = document.getElementById('backgroundcolor').value
 
+    if(x == "backgroundcolor"){
+        let boxes = document.getElementsByClassName("box");
+        [...boxes].forEach((box) =>{
+            if(box.className == "box"){box.style.backgroundColor = backgroundcolor}
+        })
+    } else {
+    removegrid();
     for (let i = 0; i < gridcounter; i++) {
     const box = document.createElement("div");
     box.className = "box";
     box.style.width = `${16/ gridsize}cm`;
     box.style.height = `${16/ gridsize}cm`;
-    box.style.backgroundColor = "rgba(255, 255, 255)"; 
+    box.addEventListener("mouseover", colorchange);
+    box.addEventListener("mousedown", colorchange);
+    box.style.backgroundColor = backgroundcolor;
     document.getElementById("quadro").appendChild(box);
-    }
+    }}
+}
+
+function colorchange (e) {
+    if (e.type === 'mouseover' && !mouseDown) return
+    let color = document.getElementById('color').value;
+    e.target.style.backgroundColor = color;
+    e.target.classList.add('colorido');
 }
 
 function slidenumbers (x) {
